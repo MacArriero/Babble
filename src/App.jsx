8d24c21730e1f0d00c7f011b472421944ff24d0b
MacArriero <s3812132@student.rmit.edu.au>
2023-04-08 22:36:11 +1000

    added Babble to routes

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/">
+        <Route path="/Babble/">
           <Route
             index
             element={
